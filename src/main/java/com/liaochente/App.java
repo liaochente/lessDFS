102080113085c40a6a415aefc7a88518e98aa2c6
liao_chente <liao_chente@hopedove.com>
2020-05-08 09:33:48 +0800

    初始化项目

---
+++ src/main/java/com/liaochente/App.java
@@ -0,0 +1,13 @@
+package com.liaochente;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
