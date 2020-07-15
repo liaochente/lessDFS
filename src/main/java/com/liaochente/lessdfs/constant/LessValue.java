7050fc00b5da472739a5f31126009df32391d093
liao_chente <liao_chente@hopedove.com>
2020-07-15 14:21:53 +0800

    refactor: 优化 Readme，增加部分代码注释

--- src/main/java/com/liaochente/lessdfs/constant/LessValue.java
+++ src/main/java/com/liaochente/lessdfs/constant/LessValue.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 用于自动映射Less服务配置项
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface LessValue {
